d6ce1aee2acdf624da74b3005c8a16c6b9c66d31
Cristian Iosif <cristian.iosif@sware.com>
2023-12-22 20:48:51 +0200

    Add timestamps info

--- resources/views/migration-timestamps-notification.blade.php
+++ resources/views/migration-timestamps-notification.blade.php
@@ -1,6 +1,6 @@
-<div class="px-4 py-3 border rounded-lg" role="alert" style="border-color: rgba(var(--warning-500));">
+<div class="px-4 py-3 border rounded-lg" role="alert" style="border-color: rgba(var(--primary-500));">
     <div class="flex items-center">
-        <x-filament::icon icon="heroicon-s-information-circle" class="flex-shrink-0 w-6 h-6" style="color: rgba(var(--warning-500));" />
+        <x-filament::icon icon="heroicon-s-information-circle" class="flex-shrink-0 w-6 h-6" style="color: rgba(var(--primary-500));" />
 
         <div class="text-sm px-4">
             If you need timestamps, you need to set them manually. They are not set by default.
